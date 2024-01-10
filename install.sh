d23ceede90b90f3f24187f2200dd6f9aeb790df2
Siddharth Sharma <75090218+PanditSiddharth@users.noreply.github.com>
2024-01-10 23:38:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
+mkdir -p /home/troot/{bin,sbin,usr/bin,usr/{bin,include,local,lib/{gcc,python3.10,x86_64-linux-gnu}},lib,lib64,home/me,proc,home/usercmp,tmp/tsx-1003,mnt,temp,var,etc/ssl,dev,root}
 cp /etc/resolv.conf /home/troot/etc/
+mknod -m 666 /home/troot/dev/null c 1 3
+cp ~/.bashrc /home/troot/home/usercmp 
+cp -r ~/.nvm /home/troot/home/usercmp 
